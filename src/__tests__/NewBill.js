501d714667fe68f550981193cd36e52916985dd7
ColineC <coline.cantelou@gmail.com>
2023-11-24 19:13:20 +0100

    new bill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,33 +2,32 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen, render} from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("I cannot send an empty new bill", () => {
-      $.fn.modal = jest.fn()
-      document.body.innerHTML = NewBillUI()
-      const formNewBill = screen.getAllByTestId('form-new-bill')
+    test("I cannot send an empty new bill", async () => {
+      const mockFunction = jest.fn();
+      document.body.innerHTML = NewBillUI();
+      const formNewBill = screen.getByTestId('form-new-bill');
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
+
       const testingNewBills = new NewBill({
         document,
         onNavigate,
         store: null,
         localStorage: null,
-      })
-      
-      const handleSubmit = jest.fn(
-        testingNewBills.handleSubmit()
-      );
+      });
+      const handleSubmit = jest.fn((e) => e.preventDefault());
       formNewBill.addEventListener("submit", handleSubmit);
-      userEvent.click(formNewBill)
-      console.log(formNewBill);
-    })
-  })
-})
+      fireEvent.submit(formNewBill);
+
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+  });
+});
